f52fa2751f049d34320afe385b92378a29447e61
khushraj <khushrajsingh13s@gmail.com>
2025-08-22 16:10:10 +0530

    Updated StoreContextProvider to use .env API URL and prop validation

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // loads variables from .env
+
+const mongoUri = process.env.MONGO_URI;
 
 export const connectDb = async () => {
   try {
-    await mongoose.connect('mongodb+srv://khushrajsingh13s:UuS5McGxANe2mL0v@cluster0.k3jow.mongodb.net/food-del');
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
--- frantend/src/components/AppDowload/AppDowload.jsx
+++ frantend/src/components/AppDowload/AppDowload.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
 import './AppDowload.css'
 import { assets } from '../../assets/assets'
-function AppDowload() {
-  return (
-    <div className='app-download' id='app-download'>
-      <p>For Better Experience Download <br /> Tomato App</p>
-      <div className='app-download-platform'>
-        <img src={assets.play_store} alt="" /><img src={assets.app_store} alt="" />
-      </div>
-    </div>
-  )import React from 'react'
-import './AppDownload.css'
-import { assets } from '../../assets/assets'
 
 function AppDownload() {
   return (
@@ -38,7 +27,3 @@ function AppDownload() {
 }
 
 export default AppDownload
-
-}
-
-export default AppDowload
